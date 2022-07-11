fc48a2f84dabc45b0929597521c6956ef70bbf62
Fachi-r <farairubvuta@gmail.com>
2022-07-11 04:44:25 +0200

    styling at breakpoints added

--- script.js
+++ script.js
@@ -1,23 +1,43 @@
+let body = document.querySelector(".hero");
 let menu = document.querySelector(".menu");
 let navList = document.querySelector(".top-nav");
 let links = document.querySelectorAll(".nav-link");
 let currentLink = document.querySelector(".nav-link-inverted");
-let body = document.querySelector(".hero");
+let images = document.querySelectorAll("img");
+let media = window.matchMedia("(min-width: 376px)");
+
+// Setting mobile || desktop images based on screen size
+function setImgSrc(_media) {
+   if (_media.matches) {
+      images.forEach((image) => {
+         const newImageSrc = image.src.replace("mobile", "desktop");
+         image.src = newImageSrc;
+      });
+   } else {
+      images.forEach((image) => {
+         const newImageSrc = image.src.replace("desktop", "mobile");
+         image.src = newImageSrc;
+      });
+   }
+}
 
 function openMenu() {
-  navList.classList.toggle("open");
+   navList.classList.toggle("open");
 }
+
+links.forEach((link) =>
+   link.addEventListener("click", (e) => {
+      {
+         if (e.currentTarget.classList.contains("nav-link-inverted")) return;
+         e.currentTarget.classList.toggle("nav-link-inverted");
+         currentLink.classList.replace("nav-link-inverted", "nav-link");
+         currentLink = e.currentTarget;
+         links = document.querySelectorAll(".nav-link");
+      }
+   })
+);
 
 menu.addEventListener("click", openMenu);
 
-links.forEach((link) =>
-  link.addEventListener("click", (e) => {
-    {
-      if (e.currentTarget.classList.contains("nav-link-inverted")) return;
-      e.currentTarget.classList.toggle("nav-link-inverted");
-      currentLink.classList.replace("nav-link-inverted", "nav-link");
-      currentLink = e.currentTarget;
-      links = document.querySelectorAll(".nav-link");
-    }
-  })
-);
+setImgSrc(media);
+media.addListener(setImgSrc);
